91d0bded21b86c79222afd81d6222d4d5a632f9a
Santhos <santhos.baala@gmail.com>
2025-04-13 21:10:05 +0200

    add tanstack router

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,8 +1,13 @@
 import tailwindcss from "@tailwindcss/vite"
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [
+    tailwindcss(),
+    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+    react(),
+  ],
 })
